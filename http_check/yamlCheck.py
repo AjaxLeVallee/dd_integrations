1c93a70927be00ecadec83c2134f344dcdbc47c0
Aaron R. Anderson <aaron.anderson.ajax@gmail.com>
2020-03-14 09:34:32 -0400

    Adding Make File, continuing to build yaml, adding comments to .gitignore

--- http_check/yamlCheck.py
+++ http_check/yamlCheck.py
@@ -3,26 +3,42 @@ import click
 from datadog_checks.base import AgentCheck
 
 
-@click.group()
-def cli():
-    print("Checked")
-    pass
+class Config(object):
 
+    """Some config options to carry along the program"""
+
+    def __init__(self):
+        """Defining initialized variables"""
+        self.verbose = False
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
+@click.group()
+@click.option('--verbose', '-v', is_flag=True)
+@pass_config
+def cli(config, verbose):
+    config.verbose = verbose
 
 @cli.command()
 @click.option('-l','--validate',
     help="Will only validate file")
 @click.argument("log_file", required=False)
-def check(validate):
-    """Will validate then create a corresponding Datadog Synthetic \
-check for each check in the file"""
+@pass_config
+def check(config, validate):
+    """Will validate then create a corresponding Datadog Synthetic check for \
+each check in the file"""
     click.echo("CLI Checker")
 
 
 @cli.command()
-def monitor():
+@pass_config
+def monitor(config):
     """Will validate or raise errors in datadog yaml file"""
-    pass
+    if config.verbose:
+        click.echo("Increasing Verbosity")
+    click.echo("Monitoring")
 
 
 #class http_check(AgentCheck):
